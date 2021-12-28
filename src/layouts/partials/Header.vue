e0d0d1385a08b56881bb873cc17bf1e844befd4b
Arnaud JOYE <joye.arnaud@gmail.com>
2021-12-28 16:33:00 +0100

    :feat: header pretest

--- src/layouts/partials/Header.vue
+++ src/layouts/partials/Header.vue
@@ -2,7 +2,18 @@
   <div>
     <header class="layout-header">
       <nav class="nav">
-        <ToggleTheme />
+        <div class="row justify-content-start">
+          <div class="menu col-1">LOGO</div>
+          <div class="menu col-10">
+            <g-link class="navlink" to="/">HOME</g-link>
+            <g-link class="navlink" to="/">ABOUT</g-link>
+            <g-link class="navlink" to="/experience">EXPERIENCE</g-link>
+            <g-link class="navlink" to="/">THOUGHTS</g-link>
+          </div>
+          <div class="col-1 switch">
+            <ToggleTheme />
+          </div>
+        </div>
       </nav>
     </header>
   </div>
@@ -19,12 +30,33 @@ export default {
 
 <style>
 .layout-header {
-  width: 100%;
+  width: 103%;
+  font-family: "Stylish";
+  font-size: 1.2em;
+  text-align: center;
+  line-height: 1.4em;
   margin: 0px auto;
-  margin-top: 30px;
+  margin-top: 10px;
 }
 .nav {
-  display: flex;
-  justify-content: flex-end;
+}
+.row {
+  width: 100%;
+}
+.menu {
+  text-align: center;
+}
+.menu .navlink {
+  padding-right: 1em;
+  color: var(--text-color);
+  text-decoration: none;
+}
+.menu .navlink:hover {
+  padding-right: 1em;
+  color: var(--border-color);
+  text-decoration: underline;
+}
+.switch {
+  text-align: right;
 }
 </style>
