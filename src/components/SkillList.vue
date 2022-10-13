a6252db9cb2f27675f83ec6822c0a59039ba9aba
Arnaud JOYE <joye.arnaud@gmail.com>
2022-10-13 15:34:36 +0200

    💄 update the skilllist component

--- src/components/SkillList.vue
+++ src/components/SkillList.vue
@@ -1,12 +1,63 @@
 <template>
-  <section>
-    <div v-for="skill in skillMap">
+  <div class="list">
+    <div>
+      <b-table
+        striped
+        hover
+        responsive="sm"
+        label-sort-asc=""
+        label-sort-desc=""
+        label-sort-clear=""
+        :items="items"
+        :fields="fields"
+        :sort-by.sync="sortBy"
+        :sort-desc.sync="sortDesc"
+        show-empty
+        small
+      >
+        <!-- A custom formatted header cell for field 'name' -->
+        <template #head(skill)="data">
+          <span class="head head-skill">{{ data.label.toUpperCase() }}</span>
+        </template>
+
+        <template #head(category)="data">
+          <span class="head head-category">{{ data.label.toUpperCase() }}</span>
+        </template>
+
+        <template #head(keywords)="data">
+          <span class="head head-keywords">{{ data.label.toUpperCase() }}</span>
+        </template>
+
+        <!-- Custom formatted column -->
+        <template #cell(skill)="data">
+          <span class="col col-skill">
+            <g-link :to="data.item.path">{{
+              data.value.toUpperCase()
+            }}</g-link></span
+          >
+        </template>
+
+        <template #cell(category)="data">
+          <span class="col col-category">{{ data.value }}</span>
+        </template>
+
+        <template #cell(keywords)="data">
+          <span class="col col-keywords">{{ data.value }}</span>
+        </template>
+      </b-table>
+      <div>
+        Sorting By: <b>{{ sortBy }}</b
+        >, Sort Direction:
+        <b>{{ sortDesc ? "Descending" : "Ascending" }}</b>
+      </div>
+    </div>
+    <!-- <div v-for="skill in skillMap">
       <h2>{{ skill[0] }}</h2>
       <div v-for="version in skill[1].versions">
         <span>{{ version }}</span>
       </div>
-    </div>
-  </section>
+    </div> -->
+  </div>
 </template>
 
 <script>
@@ -14,7 +65,21 @@ export default {
   data() {
     return {
       skillMap: null,
+      sortBy: "category",
+      sortDesc: false,
+      fields: [
+        { key: "skill", sortable: true },
+        { key: "category", sortable: true },
+        { key: "keywords", sortable: false },
+      ],
+      items: [],
     };
+  },
+  methods: {
+    onRowClicked(item, index, event) {
+      // window.location = item.path;
+      this.$router.push(item.path).catch(() => {});
+    },
   },
   async mounted() {
     try {
@@ -34,18 +99,34 @@ export default {
               ].sort();
               skillMap.set(k.skill.name, {
                 path: k.skill.path,
+                category: k.skill.category,
                 versions: newVersions,
               });
             } else {
               skillMap.set(k.skill.name, {
                 path: k.skill.path,
+                category: k.skill.category,
                 versions: k.versions.sort(),
               });
             }
           }
         });
       });
-      console.log(skillMap.keys(), "skillMap");
+      const res = [];
+      skillMap.forEach((value, key, map) => {
+        res.push({
+          skill: key,
+          path: value.path,
+          category: value.category.name,
+          keywords: value.versions.join(", "),
+        });
+      });
+
+      this.items = res;
+      // const newLocal = Array.from(skillMap);
+      // console.log(newLocal, "skillList");
+      // newLocal.flat();
+
       this.skillMap = skillMap;
     } catch (error) {
       console.log(error);
@@ -60,7 +141,7 @@ export default {
       edges{
         node{
           keywords{
-            skill{id name path}
+            skill{id name category {name} path}
             versions
           }
         }
@@ -70,9 +151,20 @@ export default {
   </static-query>
 
 <style scoped>
-#word-cloud {
-  margin-top: 15em;
-  margin-bottom: 10em;
-  width: 100%;
+#list {
+  background-color: red;
+}
+
+.head {
+  color: var(--text-color);
+}
+.col {
+  color: var(--text-color);
+}
+
+.col-skill {
+  all: initial;
+  font-weight: bold;
+  color: var(--border-color);
 }
 </style>
