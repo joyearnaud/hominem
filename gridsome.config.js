a43e6835ba2bca61b8aa045a8a3e53f430d1f0ba
Arnaud JOYE <joye.arnaud@gmail.com>
2022-11-04 19:06:32 +0100

    👷 add sitemap

--- gridsome.config.js
+++ gridsome.config.js
@@ -7,6 +7,7 @@ const i18n = require("./plugins/i18n");
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
+  siteUrl: 'https://cv.arnaudjoye.fr',
   siteName: 'Dev\' in Progress',
   siteDescription: 'Arnaud Joye - Fullstack engineer - Paris et périphérie',
   templates: {
@@ -45,6 +46,45 @@ module.exports = {
       }
     },
     i18n,
+    {
+      use: '@gridsome/plugin-sitemap',
+      options: {
+        output: '/fr/sitemap.xml',
+        exclude: ['/exclude-me'],
+        // config: {
+        //   '/fr': {
+        //     changefreq: 'weekly',
+        //     priority: 1,
+        //     lastmod: '2022-11-04',
+        //   },
+        //   '/fr/about': {
+        //     changefreq: 'monthly',
+        //     priority: 0.7,
+        //     lastmod: '2022-11-04',
+        //   },
+        //   '/fr/experience': {
+        //     changefreq: 'weekly',
+        //     priority: 0.7,
+        //     lastmod: '2022-11-04',
+        //   },
+        //   '/fr/experience/*': {
+        //     changefreq: 'weekly',
+        //     priority: 0.7,
+        //     lastmod: '2022-11-04',
+        //   },
+        //   'fr/thought': {
+        //     changefreq: 'weekly',
+        //     priority: 0.7,
+        //     lastmod: '2022-11-04',
+        //   },
+        //   'fr/thought/*': {
+        //     changefreq: 'weekly',
+        //     priority: 0.7,
+        //     lastmod: '2022-11-04',
+        //   }
+        // }
+      }
+    },
     {
       use: 'gridsome-plugin-robots',
       options: {
