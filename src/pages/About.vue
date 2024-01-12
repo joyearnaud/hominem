244991d90a042a3a74a363df0e604a639e9b9e8f
Arnaud JOYE <joye.arnaud@gmail.com>
2024-01-12 17:06:25 +0100

    adding fr metadata TEST2

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,9 +1,3 @@
-<head>
-  <meta property="og:title" content="The Rock" />
-  <meta property="og:type" content="video.movie" />
-  <meta property="og:url" content="https://www.imdb.com/title/tt0117500/" />
-  <meta property="og:image" content="https://ia.media-imdb.com/images/rock.jpg" />
-</head>
 <template>
   <Layout>
     <Landing :page="$t('page.about.name')" />
